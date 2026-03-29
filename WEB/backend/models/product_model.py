bc78b81d1e1083e2fc31baed2e635c45027bbfa2
Vasanthakumaran11 <vasanthakumaran0011@gmail.com>
2026-03-29 16:15:37 +0530

    project completely redefined frontend completed

--- WEB/backend/models/product_model.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel, Field
-
-class ProductCreate(BaseModel):
-    productId: str = Field(..., min_length=2, description="Unique product identifier")
-    name: str = Field(..., min_length=2, description="Name of the product")
--- WEB/backend/models/review_model.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-class ReviewCreate(BaseModel):
-    productId: str = Field(..., description="Id of the product being reviewed")
-    productName: str = Field(..., description="Name of the product being reviewed")
-    rating: float = Field(..., ge=1.0, le=5.0, description="Rating from 1.0 to 5.0")
-    review: str = Field(..., description="Review text")
